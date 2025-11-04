338c52b14aaa596f435ca4edf87a83fce001a8f7
cristianca345 <cristianca84@gmail.com>
2025-11-04 10:15:49 -0500

    Integrated frontend folder into main repository

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScraperForm from './components/ScraperForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Metric Mind Project 1</h1>
+      <ScraperForm />
+    </div>
+  );
+}
+
+export default App;
